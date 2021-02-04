fa542223c1511a456e7296df8bdb2f9953df1c00
ShemMwanza <77288876+ShemMwanza@users.noreply.github.com>
2021-02-04 17:47:31 +0300

    Delete IAP Assignment directory

--- IAP Assignment/util.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Util {			        
-            static $DB_NAME = "iap_app";
-            static $DB_USER = "root";
-            static $DB_USER_PASS = "";
-            static $SERVER_NAME = "localhost";	
-    }
-?>
